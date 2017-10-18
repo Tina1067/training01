de05808f050e727fa1a3a6b7d716e44070d75488
Silvio Haehner <silvio.haehner@x-case.de>
2017-10-18 16:49:36 +0200

    Introducing JSONModel and data binding.

--- webapp/view/App.view.js
+++ webapp/view/App.view.js
@@ -1,36 +1,54 @@
 sap.ui.jsview("view.App", {
 
 	/** Specifies the Controller belonging to this View. 
-	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
-	* @memberOf view.App
-	*/ 
-	getControllerName : function() {
+	 * In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	 * @memberOf view.App
+	 */
+	getControllerName: function() {
 		return "controller.App";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
-	* Since the Controller is given to this method, its event handlers can be attached right away. 
-	* @memberOf view.App
-	*/ 
-	createContent : function(oController) {
- 		var oSlider1 = new sap.m.Slider("Slider1", {
+	 * Since the Controller is given to this method, its event handlers can be attached right away. 
+	 * @memberOf view.App
+	 */
+	createContent: function(oController) {
+		var oSlider1 = new sap.m.Slider("Slider1", {
+			value: "{dataModel>/value1}",
 			change: oController.onSliderChange
 		});
-		
+
+		var oText1 = new sap.m.Text({
+			text: "{path: 'dataModel>/value1'}"
+		});
+
 		var oSlider2 = new sap.m.Slider("Slider2", {
+			value: "{model: 'dataModel', path: '/value2'}",
 			change: oController.onSliderChange
 		});
-		
+
+		var oText2 = new sap.m.Text({
+			text: "{dataModel>/value2}"
+		});
+
 		var oButton = new sap.m.Button({
 			tooltip: "Hallo Welt",
 			text: "Knopf"
 		});
- 		return new sap.m.Page({
+		
+		var oVLayout = new sap.m.VBox({
+			items: [
+				oSlider1,
+				oText1,
+				oSlider2,
+				oText2,
+				oButton
+			]
+		});
+		return new sap.m.Page({
 			title: "Title",
 			content: [
-				oSlider1,
-				oSlider2,
-				oButton
+				oVLayout
 			]
 		});
 	}
